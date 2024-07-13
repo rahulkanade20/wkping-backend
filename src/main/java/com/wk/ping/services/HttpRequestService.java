07266e1fc9058f718a4944bd1b42e26f5104172a
Rahul.Kanade <Rahul.Kanade@wolterskluwer.com>
2024-07-13 22:09:49 +0530

    adding multithreading capability

---
+++ src/main/java/com/wk/ping/services/HttpRequestService.java
@@ -0,0 +1,46 @@
+package com.wk.ping.services;
+
+import java.time.LocalDateTime;
+
+import com.wk.ping.models.Link;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.HttpClientErrorException;
+import org.springframework.web.client.HttpServerErrorException;
+import org.springframework.web.client.RestTemplate;
+
+@Service
+public class HttpRequestService {
+
+    private static final Logger logger = LoggerFactory.getLogger(HttpRequestService.class);
+
+    private final LinkService linkService;
+
+    private final RestTemplate restTemplate;
+
+    public HttpRequestService(LinkService linkService, RestTemplate restTemplate) {
+        this.linkService = linkService;
+        this.restTemplate = restTemplate;
+    }
+
+    public void makeRequest(Link link) {
+        logger.info("sending ping request, multithreaded version");
+        String l = link.getLink();
+        logger.info("Link Object is -> " + link.toString());
+        ResponseEntity<String> response;
+        int responseCode;
+        try {
+            response = restTemplate.exchange(l, HttpMethod.GET, null, String.class);
+            responseCode = response.getStatusCode().value();
+        } catch (HttpClientErrorException | HttpServerErrorException e) {
+            System.out.println("Catch block" + e.getStatusCode().value());
+            responseCode = e.getStatusCode().value();
+        }
+        link.setStatus_code(responseCode);
+        link.setLastPingTime(LocalDateTime.now());
+        linkService.updateLink(link);
+    }
+}
