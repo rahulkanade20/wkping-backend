6b5b72ac8ae615962b4fa76e0ba14dd8d81d4e15
Rahul.Kanade <Rahul.Kanade@wolterskluwer.com>
2024-08-03 15:24:31 +0530

    rollback to non threaded version

--- src/main/java/com/wk/ping/scheduling/ScheduledTasks.java
+++ src/main/java/com/wk/ping/scheduling/ScheduledTasks.java
@@ -1,20 +1,19 @@
 package com.wk.ping.scheduling;
 
+import java.time.LocalDateTime;
 import java.util.List;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-import java.util.concurrent.LinkedBlockingQueue;
-import java.util.concurrent.RejectedExecutionException;
-import java.util.concurrent.ThreadPoolExecutor;
-import java.util.concurrent.TimeUnit;
 
 import com.wk.ping.models.Link;
-import com.wk.ping.services.HttpRequestService;
 import com.wk.ping.services.LinkService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
+import org.springframework.web.client.HttpClientErrorException;
+import org.springframework.web.client.HttpServerErrorException;
+import org.springframework.web.client.RestTemplate;
 
 @Component
 public class ScheduledTasks {
@@ -22,110 +21,58 @@ public class ScheduledTasks {
 
     private final LinkService linkService;
 
-    private final HttpRequestService httpRequestService;
+    private final RestTemplate restTemplate;
 
-    private final ThreadPoolExecutor executorService;
-
-    public ScheduledTasks(LinkService linkService, HttpRequestService httpRequestService) {
-        log.info("ScheduledTasksConstructorCalled");
+    public ScheduledTasks(LinkService linkService, RestTemplate restTemplate) {
         this.linkService = linkService;
-        this.httpRequestService = httpRequestService;
-        this.executorService = new ThreadPoolExecutor(1, 1, 0L, TimeUnit.SECONDS, new LinkedBlockingQueue<>(10), Executors.defaultThreadFactory(), new ThreadPoolExecutor.AbortPolicy());
+        this.restTemplate = restTemplate;
     }
 
-    @Scheduled(fixedRate = 120000)
-    public void updatePingTimeMultiThreaded() {
+    @Scheduled(fixedRate = 20000)
+    public void updatePingTime() {
         List<Link> allLinks = linkService.getAllLinks();
+        System.out.println("Ping started");
         for(Link link : allLinks) {
-            try {
-                executorService.submit(() -> {
-                    httpRequestService.makeRequest(link);
-                    log.info("end of runnable task");
-                });
-            } catch(RejectedExecutionException e) {
-                log.error("Task rejected. All threads busy, waiting queue full " + "Rejected for Link Object -> " + link.toString());
-                e.printStackTrace();
-            }
+            String url = link.getLink();
+//            int statusCode = Ping(url);
+            int statusCode = makeCall(url);
+            link.setStatus_code(statusCode);
+            link.setLastPingTime(LocalDateTime.now());
+            linkService.updateLink(link);
         }
-
-        while (true) {
-            log.info("waiting for all tasks and queue to be empty");
-            if (executorService.getActiveCount() == 0 && executorService.getQueue().isEmpty()) {
-                log.info("exiting infinite while loop");
-                break;
-            }
-
-            try {
-                Thread.sleep(100); // Sleep for a short period before checking again
-            } catch (InterruptedException e) {
-                Thread.currentThread().interrupt();
-                System.err.println("Waiting thread was interrupted");
-                break;
-            }
-        }
-
-        log.info("ALL REQUESTS DONE");
-//        executorService.shutdown();
-//        try {
-//            if(!executorService.awaitTermination(10, TimeUnit.SECONDS)) {
-//                executorService.shutdownNow();
-//            }
-//        } catch (InterruptedException e) {
-//            Thread.currentThread().interrupt();
-//        } finally {
-//            if(!executorService.isTerminated()) {
-//                log.warn("Some threads/tasks not yet terminated, executing force shutdown");
-//            }
-//            executorService.shutdownNow();
-//        }
+        System.out.println("test log 1");
+        System.out.println("Ping ended");
     }
 
-//    @Scheduled(fixedRate = 20000)
-//    public void updatePingTime() {
-//        List<Link> allLinks = linkService.getAllLinks();
-////        System.out.println("Ping started");
-////        System.out.println("TESTING CI 1");
-//        for(Link link : allLinks) {
-////            String url = link.getLink();
-//////            int statusCode = Ping(url);
-////            int statusCode = makeCall(url);
-////            link.setStatus_code(statusCode);
-////            link.setLastPingTime(LocalDateTime.now());
-////            linkService.updateLink(link);
-//
-//            httpRequestService.makeRequest(link);
-//        }
-////        System.out.println("test log 1");
-////        System.out.println("Ping ended");
-//    }
+    public int makeCall(String url) {
+//        restTemplate.getMessageConverters().add(new MappingJackson2HttpMessageConverter());
+//        restTemplate.getMessageConverters().add(new StringHttpMessageConverter());
+        ResponseEntity<String> response;
+        try {
+            response = restTemplate.exchange(url, HttpMethod.GET, null, String.class);
+        } catch (HttpClientErrorException e) {
+            System.out.println("Catch block" + e.getStatusCode().value());
+            return e.getStatusCode().value();
+        } catch (HttpServerErrorException e) {
+            System.out.println("Catch block" + e.getStatusCode().value());
+            return e.getStatusCode().value();
+        }
+        System.out.println("make Call" + response.getStatusCode().value());
+        return response.getStatusCode().value();
+    }
 
-//    public int makeCall(String url) {
-//        ResponseEntity<String> response;
-//        try {
-//            response = restTemplate.exchange(url, HttpMethod.GET, null, String.class);
-//        } catch (HttpClientErrorException e) {
-//            System.out.println("Catch block" + e.getStatusCode().value());
-//            return e.getStatusCode().value();
-//        } catch (HttpServerErrorException e) {
-//            System.out.println("Catch block" + e.getStatusCode().value());
-//            return e.getStatusCode().value();
-//        }
-//        System.out.println("make Call" + response.getStatusCode().value());
-//        return response.getStatusCode().value();
-//    }
-
-//    public int Ping(String url) {
-//        try {
-//            ProcessBuilder processBuilder = new ProcessBuilder("ping", url);
-//            Process process = processBuilder.start();
-//            int exitCode = process.waitFor();
-//            if (exitCode == 0) {
-//                return 1; // ping success, service is up
-//            } else {
-//                return -1; // ping failed, service is down
-//            }
-//        } catch (Exception e) {
-//            return 0; // ping command failed to execute, service status unknown
-//        }
-//    }
+    public int Ping(String url) {
+        try {
+            ProcessBuilder processBuilder = new ProcessBuilder("ping", url);
+            Process process = processBuilder.start();
+            int exitCode = process.waitFor();
+            if (exitCode == 0) {
+                return 1; // ping success, service is up
+            } else {
+                return -1; // ping failed, service is down
+            }
+        } catch (Exception e) {
+            return 0; // ping command failed to execute, service status unknown
+        }
+    }
 }
