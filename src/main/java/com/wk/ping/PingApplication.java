1b15c7ef84031da01d19670d3f0513d2ebd4f48b
Rahul.Kanade <Rahul.Kanade@wolterskluwer.com>
2024-09-29 20:35:05 +0530

    added a .sh file for prod

--- src/main/java/com/wk/ping/PingApplication.java
+++ src/main/java/com/wk/ping/PingApplication.java
@@ -7,9 +7,7 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @SpringBootApplication
 @EnableScheduling
 public class PingApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(PingApplication.class, args);
 	}
-
 }
