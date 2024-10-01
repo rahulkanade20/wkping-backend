d5608b8b41168b8a8e64400e55287e26249f391f
Rahul.Kanade <Rahul.Kanade@wolterskluwer.com>
2024-10-01 23:07:48 +0530

    fixes

--- start.sh
+++ start.sh
@@ -12,6 +12,6 @@ docker rm wkping-frontend-app 2>/dev/null || true
 
 docker compose down
 
-docker pull registry.gitlab.com/monitoring-app1/wkping-backend:latest && IP_ADDRESS=127.0.0.1 docker-compose up -d
+docker pull registry.gitlab.com/monitoring-app1/wkping-backend:latest && docker-compose up -d
 
-docker pull registry.gitlab.com/monitoring-app1/wkping-frontend:latest && docker run -d --name wkping-frontend-app -e IP_ADDRESS=127.0.0.1 -p 3000:3000 registry.gitlab.com/monitoring-app1/wkping-frontend:latest
+docker pull registry.gitlab.com/monitoring-app1/wkping-frontend:latest && docker run -d --name wkping-frontend-app -p 3000:3000 registry.gitlab.com/monitoring-app1/wkping-frontend:latest
